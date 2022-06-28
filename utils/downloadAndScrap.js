ae6eeb3e5ad12b06fc082709b030f208f93cf1e4
Luks17 <lucasthidavort@gmail.com>
2022-06-27 21:53:12 -0400

    Improved support for manual captions

--- utils/downloadAndScrap.js
+++ utils/downloadAndScrap.js
@@ -3,15 +3,20 @@ const youtubedl = require("youtube-dl-exec");
 const { resolve } = require("path");
 const { unlink, readFile } = require("fs").promises
 
-const scrap = async (filePath) => {
+const scrap = async (filePath, isAutoGenSub) => {
   let text = await readFile(`${filePath}.en.vtt`, "utf-8");
   const regex = /(WEBVTT\nKind: captions\nLanguage: en\n\n)|((\w+\s)?<\d\d:\d\d:\d\d.\d\d\d><c>\w+(('|-)\w+)?\s?\s?<\/c>)|((\d\d:){2}\d\d\.\d{3}(\s-->\s)?(\s\w+:\w+\s\w+:\d%)?)/g
 
   text = text.replace(regex, "");
   text = text.replace(/\s\n/g, "");
-  text = text.replace(/\n/g, "");
-  text = text.replace(/\s\s/g, "\n");
 
+  if(isAutoGenSub) {
+    text = text.replace(/\n/g, "");
+    text = text.replace(/\s\s/g, "\n");
+  }
+  else {
+    text = text.slice(1); // this is needed because my regex leaves always a \n at the start
+  }
   await unlink(`${filePath}.en.vtt`);
 
   return text;
@@ -28,7 +33,10 @@ const getVideoTranscription = async (url) => {
     output: filePath
   })
 
-  return await scrap(filePath);
+  const subs = await youtubedl(url, { listSubs: true })
+  const isAutoGenSub = subs.includes("Available automatic captions");
+
+  return await scrap(filePath, isAutoGenSub);
 }
 
 module.exports = getVideoTranscription;
